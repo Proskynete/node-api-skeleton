d371865a487538f4dffdad2dfc37a0d389eb12e0
Eduardo Esteban Alvarez Castañeda <eduardo.a1993@gmail.com>
2023-01-18 00:52:36 -0300

    add: create hellos's model

--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -4,6 +4,24 @@ import { HelloController } from "../controllers/hello";
 
 const router = Router();
 
+/**
+ * @openapi
+ * /api/v1/hello:
+ *  get:
+ *    tags:
+ *    - Example
+ *    responses:
+ *      200:
+ *        description: First dummy route - Route example
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Hello World!
+ */
 router.get("/", HelloController);
 
 export { router };
