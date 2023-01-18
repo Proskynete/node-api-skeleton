722bb3ff5c086f6d79e13c959d8712db0fc70e74
Eduardo Esteban Alvarez Castañeda <eduardo.a1993@gmail.com>
2023-01-18 01:04:03 -0300

    add: create services folder and create Hello service

--- src/controllers/hello.ts
+++ src/controllers/hello.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
 
 import { HelloService } from "../services/hello";
+import { handleHttpError } from "../utils/error";
 
 export const HelloController = async (req: Request, res: Response) => {
-  const response = await HelloService();
-  res.status(200).json(response);
+  try {
+    const response = await HelloService();
+    res.status(200).json(response);
+  } catch (error) {
+    handleHttpError(res, "[HelloController] - Error: ", error);
+  }
 };
